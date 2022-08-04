69be9f8d52732ffaed2c73e61def628490cbf2a0
DanielSTS <daniel.junior.ifpb@gmail.com>
2022-08-03 22:56:37 -0300

    test: missing param: email

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,9 +1,18 @@
 export class SignUpController {
   handle(httpRequest) {
-    return {
-      statusCode: 400,
+    if (!httpRequest.body.name) {
+      return {
+        statusCode: 400,
+        body: new Error("Missing param: name"),
+      };
+    }
 
-      body: new Error("Missing param: name"),
-    };
+    if (!httpRequest.body.email) {
+      return {
+        statusCode: 400,
+
+        body: new Error("Missing param: email"),
+      };
+    }
   }
 }
