f5038d6d7c63c6450d2be132a748607ca5011684
DanielSTS <daniel.junior.ifpb@gmail.com>
2022-08-08 23:11:02 -0300

    test: ensure DbAddAccount Should throw if encrypter throws

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -38,16 +38,19 @@ describe("DbAddaccount Usecase", () => {
     expect(encryptSpy).toHaveBeenCalledWith("valid_password");
   });
 
-  // describe("DbAddaccount Usecase", () => {
-  //   test("Should call encrypter with correct password", async () => {
-  //     const { sut, encrypterStub } = makeSut();
-  //     const encryptSpy = jest.spyOn(encrypterStub, "encrypt");
-  //     const accountData = {
-  //       name: "valid_name",
-  //       email: "valid_email",
-  //       password: "valid_password",
-  //     };
-  //     await sut.add(accountData);
-  //     expect(encryptSpy).toHaveBeenCalledWith("valid_password");
-  //   });
+  test("Should throw if encrypter throws", async () => {
+    const { sut, encrypterStub } = makeSut();
+    const encryptSpy = jest
+      .spyOn(encrypterStub, "encrypt")
+      .mockReturnValueOnce(
+        new Promise((resolve, reject) => reject(new Error()))
+      );
+    const accountData = {
+      name: "valid_name",
+      email: "valid_email",
+      password: "valid_password",
+    };
+    const promise = sut.add(accountData);
+    expect(promise).rejects.toThrow();
+  });
 });
