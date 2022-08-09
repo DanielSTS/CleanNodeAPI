015aae41b6bf2b568a4a54c493aee4116f077153
DanielSTS <daniel.junior.ifpb@gmail.com>
2022-08-09 00:30:36 -0300

    test: ensure Should return a hashed on sucess

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -1,5 +1,12 @@
 import bcrypt from "bcrypt";
+import { resolve } from "path";
 import { BcryptAdapter } from "./bcrypt-adapter";
+
+jest.mock("bcrypt", () => ({
+  async hash(): Promise<string> {
+    return new Promise((resolve) => resolve("hash"));
+  },
+}));
 
 const makeSut = (salt: number): BcryptAdapter => {
   return new BcryptAdapter(salt);
@@ -12,5 +19,12 @@ describe("Bcrypt Adapter", () => {
     const hashSpy = jest.spyOn(bcrypt, "hash");
     await sut.encrypt("any_value");
     expect(hashSpy).toHaveBeenCalledWith("any_value", salt);
+  });
+
+  test("Should return a hashed on sucess", async () => {
+    const salt = 12;
+    const sut = makeSut(salt);
+    const hash = await sut.encrypt("any_value");
+    expect(hash).toBe("hash");
   });
 });
